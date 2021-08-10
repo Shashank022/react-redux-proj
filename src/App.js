c6e6b4045ac4074d50047f6093da0339570aa9d7
Shashank <shashank022@gmail.com>
2021-08-10 13:07:29 -0500

    add redux store changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./containers/Header";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProductListing from "./containers/ProductListing";
@@ -8,13 +7,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header>
+        <Header/>
           <Switch>
           <Route path="/" exact component={ProductListing}></Route>
           <Route path="/product/:productId" exact component={ProductDetails}></Route>
           <Route>404 Not Found..!!</Route>
           </Switch>
-        </Header>
       </Router>
     </div>
   );
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import ProductComponent from './ProductComponent';
 
 const ProductListing = () =>{
 const products = useSelector((state) => state);
 console.log(products);
     return(
-        <div className="ui grid container">
-            <h1>Product Listing</h1>
+        <div className="four column wide">
+            <div className="ui link cards">
+                <div className="card">
+                    <ProductComponent/>
+            </div>
+            </div>
+           
         </div>
     )
 }
