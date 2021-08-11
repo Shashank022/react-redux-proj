2c5a08f8c10490258f1b216c06a179d3cdbbb2e5
Shashank <shashank022@gmail.com>
2021-08-11 14:40:16 -0500

    added changes

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ProductComponent = () =>{
     const products = useSelector((state) => state.allProducts.products);
@@ -7,17 +8,19 @@ const ProductComponent = () =>{
         const {id,title, price, image, category} = product;
     return(
             <div className="four wide column" key={id}>
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image">
-                        <img src={ image} alt={title}/></div>
-                        <div className="content">
-                            <div className="header">{title}</div>
-                            <div className="meta price">$ {price}</div>
-                            <div className="meta">{category}</div>
+                <Link to={`/product/${id}`}>
+                    <div className="ui link cards">
+                        <div className="card">
+                            <div className="image">
+                                <img src={ image} alt={title}/></div>
+                                <div className="content">
+                                    <div className="header">{title}</div>
+                                    <div className="meta price">$ {price}</div>
+                                    <div className="meta">{category}</div>
+                                </div>
                         </div>
-                </div>
-            </div>
+                    </div>
+            </Link>
         </div>);
     });
 
--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectedProduct } from '../redux/actions/productActions';
+
+
 
 const ProductDetails = () =>{
+    const product = useSelector((state) => state.product);
+    const {productId} = useParams();
+    console.log(productId);
+    const dispatch = useDispatch();
+
+const fetchProductDetails = async() =>{
+    const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
+    .catch((err)=>{
+        console.log(err);
+    });
+    console.log(response);
+    dispatch(selectedProduct(response.data));
+}
 
     return(
         <div>
